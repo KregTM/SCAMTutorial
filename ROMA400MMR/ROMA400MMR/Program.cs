9af57940eae52bed5950a21c4c170b741f4a1e03
Nikolai <71208972+KregTM@users.noreply.github.com>
2021-09-12 15:24:59 +0600

    DOta 1
    дорогой дневник, мне не передать словами ту боль, что я ощущаю сейчас. Релиз доты 2 перенесли на неделю?

---
+++ ROMA400MMR/ROMA400MMR/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROMA400MMR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   /*
+            String mString = "Name";
+            Enemy myEnemy = new Enemy();
+            
+
+            myEnemy.Attack();
+            myEnemy.Move();
+            */
+            AngryDoter myAngryDoter = new AngryDoter();
+            myAngryDoter.name = "Arseniy";
+            myAngryDoter.Attack();
+            myAngryDoter.Move();
+            
+
+            Console.WriteLine("Doter name: " + myAngryDoter.name);
+            Console.ReadLine();
+        }
+    }
+
+
+    class Enemy
+        {
+        public int damage;
+        public String name;
+
+        /*public Enemy(int enemyDamage, String enemyName)
+        {
+            damage = enemyDamage;
+            name = enemyName;                                          
+        }*/
+
+        public void Attack()
+        {
+            Console.WriteLine("Baza kormit detey svoih");
+        }
+
+        virtual public void Move()
+        {
+            Console.WriteLine("Baza dvishet detey svoih");
+        }
+       
+    }
+    
+    class AngryDoter : Enemy
+    {
+        public override void Move()
+        {
+            Console.Write("Angry Doter move");
+        }
+    }
+}
